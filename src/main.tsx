9b2be6c29df71aa22eb79acea3b671a376f0445d
Jorge Enrrique Fonseca Murillo <jorge.fonseca22f@gmail.com>
2026-03-12 11:15:44 -0500

    Configuracion inicial del proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Button } from "@/components/ui/button";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <h1 className="text-3xl font-bold underline">pagina principal</h1>
+    <h1 className="text-3xl font-bold underline">pagina principal</h1>
+    <Button>Boton</Button>
+    <h2 className="text-2xl font-semibold tracking-tight text-blue-600 border-b pb-2">
+      Probando Configuración
+    </h2>
+
+    <p className="text-sm text-slate-500 italic">
+      Si ves este texto azul y con borde, Tailwind está activo.
+    </p>
   </StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 2. Define que @ apunta a /src
+    },
+  },
+});
